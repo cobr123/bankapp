040f19bd485f1b3fff59656712a1ba44d521676d
safebuster2 <safebuster2@gmail.com>
2025-08-15 11:41:37 +0300

    Unable to find GatewayFilterFactory with name TokenRelay

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-consul-discovery")
     implementation("org.springframework.cloud:spring-cloud-starter-consul-config")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 }
 
 tasks.withType<Test> {
