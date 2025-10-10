1c15d5e23622ef7866c1c03a737858e6cb2cdbc5
safebuster2 <safebuster2@gmail.com>
2025-10-10 23:23:20 +0300

    добавил logstash и elasticsearch

--- build_images.sh
+++ build_images.sh
@@ -16,6 +16,8 @@ docker build -t kafka:latest ./kafka
 docker build -t zipkin:latest ./zipkin
 docker build -t prometheus:latest ./prometheus
 docker build -t grafana:latest ./grafana
+docker build -t logstash:latest ./logstash
+docker build -t elasticsearch:latest ./elasticsearch
 docker build -t kibana:latest ./kibana
 
 echo "Docker images built successfully!"
